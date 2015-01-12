b2106990b36c5dd657c92d8be206ecc460e5a1e5
alancodvo <alancodvo@gmail.com>
2015-01-12 09:33:32 +0900

    Initial Commit

---
+++ SleepTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SleepTimer
+//
+//  Created by alan-arakawa-yoshihiro on H27/01/12.
+//  Copyright (c) 平成27年 Yoshihiro Arakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SleepTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SleepTimer
+//
+//  Created by alan-arakawa-yoshihiro on H27/01/12.
+//  Copyright (c) 平成27年 Yoshihiro Arakawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
